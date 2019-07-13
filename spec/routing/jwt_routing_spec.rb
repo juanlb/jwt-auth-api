5b0b1a593d436b107d9f593ba854d48f6479d4c4
Joao <juan.labattaglia@snappler.com>
2019-07-13 18:16:17 -0300

    add jwt routing spec

---
+++ spec/routing/jwt_routing_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::JwtController, type: :routing do
+  describe 'routing' do
+    it 'routes to #auth' do
+      expect(post: '/api/v1/jwt/auth').to route_to('api/v1/jwt#auth')
+    end
+    
+    it 'routes to #refresh' do
+      expect(post: '/api/v1/jwt/refresh').to route_to('api/v1/jwt#refresh')
+    end
+
+    it 'routes to #valid' do
+      expect(post: '/api/v1/jwt/valid').to route_to('api/v1/jwt#valid')
+    end
+  end
+end
